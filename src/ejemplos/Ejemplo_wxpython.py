613f79b1708b611e922c0e41e13e4e4f2a9d742d
gvilchis23 <gvilchis23@gmail.com>
2013-02-11 21:09:25 -0600

    cambios la configuracion de wxpython y un ejemplo

---
+++ src/ejemplos/Ejemplo_wxpython.py
@@ -0,0 +1,16 @@
+'''
+Created on 11/02/2013
+
+@author: infest48
+'''
+#!/usr/bin/python
+# simple.py
+
+import wx
+
+app = wx.App()
+
+frame = wx.Frame(None, -1, 'simple.py')
+frame.Show()
+
+app.MainLoop()
